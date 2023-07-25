ed37accff13aeb66be1fad6448adb292d21aa999
이진국 <jinkook0126@naver.com>
2023-07-26 00:44:01 +0900

    feat: 린트 설정

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/semi': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+    'react/function-component-definition': [
+      'warn',
+      { namedComponents: 'arrow-function', unnamedComponents: 'arrow-function' },
+    ],
+  },
+  // overrides: [
+  //   {
+  //     files: ['tests/**/*', '**/__tests__/**/*', '**/*.{spec,test}.*'],
+  //     env: {
+  //       'jest/globals': true,
+  //     },
+  //     plugins: ['jest', 'jest-dom', 'testing-library'],
+  //     extends: ['plugin:jest/recommended', 'plugin:jest-dom/recommended', 'plugin:testing-library/react'],
+  //     rules: {
+  //       'jest/expect-expect': 'off',
+  //     },
+  //   },
+  // ],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+/**
+ * Prettier 옵션
+ * - https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  proseWrap: 'never',
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: true,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+};

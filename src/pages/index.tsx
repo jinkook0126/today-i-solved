19346d51da2659d2ee45541215cea9089e3f43a9
이진국 <jinkook0126@naver.com>
2023-08-26 18:22:11 +0900

    feat: getServerSideProps 구조 적용 진행 중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,49 @@
-import { NextPage } from 'next';
-import NextLink from 'next/link';
+import { GetServerSideProps, NextPage } from 'next';
 import ServiceLayout from '@/components/layout/service_layout';
-import { Link } from '@chakra-ui/react';
+import { Center, Text, Box, Flex } from '@chakra-ui/react';
 
-const IndexPage: NextPage = () => {
+interface Post {
+  id: number;
+  title: string;
+}
+interface IndexProps {
+  name: string;
+  posts: Post[];
+}
+const IndexPage: NextPage<IndexProps> = props => {
+  const { posts } = props;
   return (
     <ServiceLayout>
-      <Link as={NextLink} href='/tmp'>
-        Temp 페이지 잠~~깐만 이동
-      </Link>
+      <Center h={240} bg={'teal.100'}>
+        <Text fontWeight={'bold'} fontSize={30}>
+          검색 영역
+        </Text>
+      </Center>
+      <Box w={{ base: 'calc(100% - 2rem)', '2xl': '1728px', xl: '1376px', lg: '1024px' }} marginX='auto'>
+        <Flex wrap={'wrap'}>
+          {posts.map(post => {
+            return (
+              <Box boxShadow={'tis'} key={post.id} m={'16px'} borderRadius={4} bg={'white'} h={377} flex={1}>
+                <Text fontWeight={300}>{post.title}</Text>
+              </Box>
+            );
+          })}
+        </Flex>
+      </Box>
     </ServiceLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<IndexProps> = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+  const jsonData = await response.json();
+  const posts: Post[] = jsonData;
+  return {
+    props: {
+      name: 'jinkook',
+      posts,
+    },
+  };
 };
 
 export default IndexPage;

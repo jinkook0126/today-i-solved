bfbb58234266c63e8a806c7195e94f1850ef0b07
이진국 <jinkook0126@naver.com>
2023-09-27 16:12:27 +0900

    feat: Toast UI - Editor 설치

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,12 +1,30 @@
+import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
+import Editor from '@toast-ui/editor';
+import { useRef, useEffect } from 'react';
 import WriteLayout from '@/components/layout/write_layout';
 import { Box, Text, Heading } from '@chakra-ui/react';
 import { NextPage } from 'next';
 interface PageProps {}
 
 const Write: NextPage<PageProps> = () => {
+  const editorRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (editorRef.current !== null) {
+      const editor = new Editor({
+        el: editorRef.current,
+        height: '500px',
+        initialEditType: 'markdown',
+        previewStyle: 'vertical',
+      });
+
+      editor.getMarkdown();
+    }
+  }, []);
   return (
     <WriteLayout>
-      <Box></Box>
+      <Box>
+        <div ref={editorRef}></div>
+      </Box>
     </WriteLayout>
   );
 };

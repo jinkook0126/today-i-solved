f7fe3895228336863ebc58b40a84af2ab480b6eb
이진국 <jinkook0126@naver.com>
2023-10-30 20:38:21 +0900

    feat: 글쓰기 - 제목 #1

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -2,29 +2,30 @@ import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
 import Editor from '@toast-ui/editor';
 import { useRef, useEffect } from 'react';
 import WriteLayout from '@/components/layout/write_layout';
-import { Box, Text, Heading } from '@chakra-ui/react';
+import { Box, Text, Heading, Flex, Input } from '@chakra-ui/react';
 import { NextPage } from 'next';
 interface PageProps {}
 
 const Write: NextPage<PageProps> = () => {
   const editorRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
-    if (editorRef.current !== null) {
-      const editor = new Editor({
-        el: editorRef.current,
-        height: '500px',
-        initialEditType: 'markdown',
-        previewStyle: 'vertical',
-      });
+  // useEffect(() => {
+  //   if (editorRef.current !== null) {
+  //     const editor = new Editor({
+  //       el: editorRef.current,
+  //       height: '500px',
+  //       initialEditType: 'markdown',
+  //       previewStyle: 'vertical',
+  //     });
 
-      editor.getMarkdown();
-    }
-  }, []);
+  //     editor.getMarkdown();
+  //   }
+  // }, []);
   return (
     <WriteLayout>
-      <Box>
-        <div ref={editorRef}></div>
+      <Box w={{ base: 'calc(100% - 2rem)', '2xl': '1728px', xl: '1376px', lg: '1024px' }} h='100vh' marginX='auto'>
+        <Input variant='unstyled' placeholder='제목을 입력하세요' h='66px' fontSize={'2.75rem'} fontWeight={'700'} />
       </Box>
+      <Box>{/* <div ref={editorRef}></div> */}</Box>
     </WriteLayout>
   );
 };

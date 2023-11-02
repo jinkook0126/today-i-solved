223f18327ddff282e8ba96b15989e0e7daf91fa9
이진국 <jinkook0126@naver.com>
2023-11-03 01:29:23 +0900

    feat: 글쓰기 - 태그 #2
     - 태그 레이아웃 생성

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,13 +1,14 @@
 import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
 import Editor from '@toast-ui/editor';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import WriteLayout from '@/components/layout/write_layout';
-import { Box, Text, Heading, Flex, Input } from '@chakra-ui/react';
+import { Box, Text, Heading, Flex, Input, Tag } from '@chakra-ui/react';
 import { NextPage } from 'next';
 interface PageProps {}
 
 const Write: NextPage<PageProps> = () => {
   const editorRef = useRef<HTMLDivElement>(null);
+  const [tags, setTags] = useState(['FE', '정수론']);
   // useEffect(() => {
   //   if (editorRef.current !== null) {
   //     const editor = new Editor({
@@ -20,6 +21,8 @@ const Write: NextPage<PageProps> = () => {
   //     editor.getMarkdown();
   //   }
   // }, []);
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {};
   return (
     <WriteLayout>
       <Box
@@ -44,6 +47,34 @@ const Write: NextPage<PageProps> = () => {
             borderRadius='1px'
             backgroundColor='gray.400'
           />
+          <Flex flexWrap='wrap'>
+            {tags.map(tag => (
+              <Tag
+                h={{ sm: '1.5rem', md: '2rem' }}
+                fontSize={{ sm: '0.75rem', md: '1rem' }}
+                borderRadius={{ sm: '0.75rem', md: '1rem' }}
+                paddingX={{ sm: '0.75rem', md: '1rem' }}
+                mr={{ sm: '0.5rem', md: '0.75rem' }}
+                mb={{ sm: '0.5rem', md: '0.75rem' }}
+                color={'teal'}
+                bg='gray.100'
+              >
+                {tag}
+              </Tag>
+            ))}
+            <Input
+              display='inline-flex'
+              variant='unstyled'
+              placeholder='태그를 입력하세요'
+              padding='1px'
+              w='14rem'
+              // h={{ sm: '43px', md: '66px' }}
+              h={{ sm: '2rem', md: '2.125rem' }}
+              fontSize={{ sm: '0.75rem', md: '1.125rem' }}
+              lineHeight={{ sm: '1.5rem', md: '2rem' }}
+              onKeyDown={onKeyDown}
+            />
+          </Flex>
         </Box>
       </Box>
       <Box>{/* <div ref={editorRef}></div> */}</Box>
